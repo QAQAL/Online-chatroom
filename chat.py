b18b95a7fdc35be4ac59da7829fd1f319d73be40
LAPTOP-FSUTQ4RK\Q <qaqal2017@gmail.com>
2017-09-11 19:48:55 +0800

    entrance

---
+++ chat.py
@@ -0,0 +1,10 @@
+#!/bin/env python
+from app import (
+    create_app,
+    socketio,
+)
+
+
+if __name__ == '__main__':
+    app = create_app(debug=True)
+    socketio.run(app, host='0.0.0.0', port=80)
